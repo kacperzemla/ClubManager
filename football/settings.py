d6b6278d2e7954727bca435d8792fcf0e26e95f7
Kacper <kacperpeper@gmail.com>
2022-03-09 23:30:53 +0100

    static files

--- football/settings.py
+++ football/settings.py
@@ -148,5 +148,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'/build/static'),
+    os.path.join(BASE_DIR,'build/static'),
 ]
