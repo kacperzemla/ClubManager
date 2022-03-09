b1a363510742d3c83d647b5ec7d8d01e626ac2c0
Kacper <kacperpeper@gmail.com>
2022-03-09 23:59:51 +0100

    changed the folder of the build

--- football/settings.py
+++ football/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'football.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'footballreact/build')],
+        'DIRS': [os.path.join(BASE_DIR,'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
