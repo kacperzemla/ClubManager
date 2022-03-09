972be00719232977ca41a7e74fc6991ba4672507
Kacper <kacperpeper@gmail.com>
2022-03-09 23:27:49 +0100

    static files

--- football/settings.py
+++ football/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 
@@ -148,5 +148,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'footballreact/build/static'),
+    os.path.join(BASE_DIR,'/build/static'),
 ]
